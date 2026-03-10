51bbcf1c6d78c6241e7b47335634815e3a682da1
Katie Garcia <katie.garcia.c@gmail.com>
2026-03-09 23:11:37 -0500

    Add pricing templates and update urls/views

--- backend/pricing/urls.py
+++ backend/pricing/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from .views import admin_page
-from .views import PricingQuoteView
+from .views import home, admin_page, PricingQuoteView
 from . import api_views
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin-page/", admin_page, name="admin-page"),
-    
     path("quote/", PricingQuoteView.as_view(), name="pricing-quote"),
- 
-     # Inventory API
     path("bikes/", api_views.bikes_list, name="bikes_list"),
-    # if you made an update endpoint:
     path("bikes/<int:bike_id>/", api_views.bike_update, name="bike_update"),
 ]
