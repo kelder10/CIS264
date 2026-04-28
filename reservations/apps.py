6bb1df79b803cdabf5f64f08df189a271bcff5f6
kelder10 <klang5875@gmail.com>
2026-04-27 21:16:20 -0700

    Delete reservations directory

--- reservations/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class ReservationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reservations'
-    verbose_name = 'Reservations'
-    
-    def ready(self):
-        import reservations.signals  
