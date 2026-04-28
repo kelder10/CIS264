a74febb9ddc94809a37f2501bfac885fc9ca6fca
kelder10 <klang5875@gmail.com>
2026-04-27 21:16:11 -0700

    Delete payments directory

--- payments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('process/<int:reservation_id>/', views.payment, name='payment'),
-    path('confirmation/<int:payment_id>/', views.payment_confirmation, name='payment_confirmation'),
-    path('history/', views.payment_history, name='payment_history'),
-    path('apply-promo/<int:reservation_id>/', views.apply_promo_code, name='apply_promo_code'),
-]
